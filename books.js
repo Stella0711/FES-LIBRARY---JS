57f7c3df16b0bf558632b7698afa201592429cad
Stella0711 <crystal.quigley@gmail.com>
2026-06-04 09:01:17 -0500

    start at min 4

--- books.js
+++ books.js
@@ -27,13 +27,24 @@ function renderBooks(filter) {
       ${ratingsHTML(book.rating)}
     </div>
     <div class="book__price">
-      <span>$${book.originalPrice.toFixed(2)}</span> 
+       ${priceHTML(book.originalPrice, book.salePrice)}
     </div>
   </div>`
   })
   .join("");
 
 booksWrapper.innerHTML = booksHtml;
+}
+
+function priceHTML(originalPrice, salePrice) {
+  if (!salePrice) {
+    return `$${originalPrice.toFixed(2)}`
+  }
+  else if (salePrice) {
+    }
+
+ return 'THERE IS A SALE';
+  // <span class="book__price--normal">$59.95</span> $14.95
 }
 
 function ratingsHTML(rating) {
